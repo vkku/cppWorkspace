b7d8f7e3c09a9876239c2c685b7119472d8f1583
vkku <kumar.vivekten@gmail.com>
2017-06-01 00:51:25 +0530

    Deletion any

--- LinkedListFull.cpp
+++ LinkedListFull.cpp
@@ -14,6 +14,7 @@ void printMenu();
 void createNode(node** head, node** current);
 void traverse(node* head);
 void delPos(node** head, node** current);
+void insPos(node** head, node** current);
 
 int main()
 {
@@ -36,10 +37,13 @@ int main()
             case    1   :   createNode(&head, &current);
                             break;
 
-            case    2   :   traverse(head);
+            case    2   :   insPos(&head, &current);
                             break;
 
-            case    3   :   delPos(&head, &current);
+            case    3   :   traverse(head);
+                            break;
+
+            case    4   :   delPos(&head, &current);
                             break;
 
             case    0   :   cout<<"\nExit";
@@ -63,8 +67,9 @@ void printMenu()
     cout<<"\nCHOICE     OPERATION";
     cout<<"\n";
     cout<<"\n[ 1  ]        Insert Element\n";
-    cout<<"\n[ 2  ]        Display Elements\n";
-    cout<<"\n[ 3  ]        Delete Element\n";
+    cout<<"\n[ 2  ]        Insert Element at any position\n";
+    cout<<"\n[ 3  ]        Display Elements\n";
+    cout<<"\n[ 4  ]        Delete Element at any position\n";
     cout<<"\n[ 99 ]        To print this menu again any time later\n";
     cout<<"\n[ 0  ]        Exit\n";
 }
@@ -143,9 +148,9 @@ void delPos(node** head, node** current)
         }
         //cout<<"\nPredecessor : "<<pred->info;
         //cout<<"\nSuccessor : "<<succ->info;
-        if(succ == NULL)
-        {
-            *current = pred;
+        if(succ == NULL)                        //In case last element is to be deleted
+        {                                       //Moving *current pointer to appropriate position
+            *current = pred;                    //As to facilitate further insertion
             pred -> next = NULL;
         }
         else
@@ -157,11 +162,50 @@ void delPos(node** head, node** current)
     cout<<"\nNode at Position [ "<<pos<<" ] Deleted Succesfully";
 }
 
+/*
+ToDo
+1. Check for invalid position input
 
+*/
+void insPos(node** head, node** current)
+{
+    int pos;
+    int cnt = 0;
+    node* temp = (node* )malloc(sizeof(node* ));
+    node* pred = *head;     //Predecessor to the node to be deleted
+    node* succ = pred;      //Successor to the node to be deleted
+    cout<<"\nEnter Position\n";
+    cin>>pos;
+    cout<<"\nEnter Element\n";
+    cin>>temp -> info;
+    temp -> next = NULL;
 
+    if(pos == 1)
+    {
+        temp -> next = *head;
+        *head = temp;
 
+        if((*head) -> next == NULL)
+        {
+            *current = *head;           //In case user enters 1st element by this method
+        }                               //But 2nd with Insert Element option
+    }
 
+    else
+    {
+        //Finding Predecessor and Successor
+        while(cnt++ < pos-2)
+        {
+            pred = pred -> next;
+        }
 
+        succ = pred -> next;
+
+        pred -> next = temp;
+        temp -> next = succ;
+    }
+
+}
 
 
 
